dc057fd6892d16afd21876f2b76bc97c141f2c24
0xkatana <m_kara@estin.dz>
2025-12-02 21:26:27 +0100

    feat : day 1 challenge solution with python

---
+++ day1/py-sol/solve.py
@@ -0,0 +1,24 @@
+
+dial = 50 
+rotations = []
+password = 0 
+with open("input.aoc") as f : 
+    for line in f :
+        rotations.append(line.strip())
+        
+for r in rotations :
+    #print(r[0]) 
+    if r[0] == "R":
+        print(f"[Dial] {dial} Rotating right by {int(r[1:])}")
+        dial += int(r[1:])
+        dial = dial % 100
+    else: 
+        print(f"[Dial] {dial}  Rotating left by {int(r[1:])}")
+        dial -= int(r[1:])
+        dial = dial % 100
+    print(f"[UPDATED DIAL] {dial}")
+    if dial == 0 : 
+            password +=1
+print(dial)
+print(password)
+#print(rotations)

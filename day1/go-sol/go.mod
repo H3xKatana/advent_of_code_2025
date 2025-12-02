b08ac531224df1ba117e103368157c8a159d94de
0xkatana <m_kara@estin.dz>
2025-12-02 23:46:48 +0100

    feat : day 1 challenge solution with go

---
+++ day1/go-sol/go.mod
@@ -0,0 +1,3 @@
+module github.com/H3xKatana/advent_of_code_2025/day1/go-sol
+
+go 1.22.2
---
+++ day1/go-sol/solve.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strconv"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+
+	file , err	:= os.Open("input.txt")
+	dial := 50
+	rotations := []string{}
+	password := 0
+	check(err)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+        line := scanner.Text()
+        rotations = append(rotations,line)
+    }
+	for _ , r := range rotations {
+		rotation, err := strconv.Atoi(r[1:])
+		check(err)
+
+		if r[0] == 'R' {
+			dial += rotation
+			dial = dial % 100
+		} else {
+			dial -= rotation
+			dial = dial % 100
+		}
+
+		if dial == 0 {
+			password += 1
+		}
+
+	}
+
+	fmt.Println(password)
+	//fmt.Println(strings.Join(rotations, ", "))
+    fmt.Println("Go-sol - by 0xkatana")
+}
